1a18641c1cf5dcba26da5eb7d372a80a23b58335
Rafael Kazuhito Vidal Beltrão <rafakazuhito@gmail.com>
2023-11-22 17:30:28 -0300

    php

--- fale.php
+++ fale.php
@@ -1,4 +1,31 @@
-<?php
-    $login=$_POST["login"];
-    echo $login;
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <link rel="shortcut icon" href="imagens/favicon.ico" type="image/x-icon">
+    <title>Fale Conosco</title>
+</head>
+<body>
+    <header>
+        <h1>Marcelo Pires Vieira</h1>
+        <p>Mais conhecido pelo seu nome artístico Belo, aqui você encontrará tudo sobre esse belo cantor</p>
+    </header>
+    <nav>
+        <a href="index.html">Home</a>
+        <a href="fale.html">Fale Conosco</a>
+        <a href="vetor.html">Vetores</a>
+        <a href="#">Curiosidades</a>
+    </nav>
+    <main>
+        <?php
+            $login=$_POST["login"];
+            echo $login;
+        ?>
+    </main>
+    <footer>
+        <p>Site criado por Matheus C., Patrick P. e Rafael K.</a></p>
+    </footer>
+</body>
+</html>
