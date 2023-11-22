a3b75cf8fb93cfb49c0de6c0d1ea9244aa425fe7
Rafael Kazuhito Vidal Beltrão <rafakazuhito@gmail.com>
2023-11-22 18:29:11 -0300

    update

--- fale.php
+++ fale.php
@@ -16,12 +16,25 @@
         <a href="index.html">Home</a>
         <a href="fale.html">Fale Conosco</a>
         <a href="vetor.html">Vetores</a>
-        <a href="#">Curiosidades</a>
+        <a href="curiosidades.html">Curiosidades</a>
     </nav>
     <main>
         <?php
-            $nome=$_POST["nome"];
-            echo $login;
+            $nome=$_POST['nome'];
+            $email=$_POST['email'];
+            $cel=$_POST['cel'];
+            $hora=$_POST['hora'];
+            $data=$_POST['data'];
+            $password=$_POST['password']
+            $musica=$_POST['musica']
+            $sexo=$_POST['sexo']
+            $estado=$_POST['estado']
+            $mensagem=$_POST['msg']
+             <textarea name="msg" id="" cols="50" rows="10"></textarea>
+             <br>
+             Nivel de contentamento: 0<input type="range" name="contente" min="1" max="100">100
+             <br>
+             <input style="display: block; margin: auto; font-size: 1.2em;" type="submit" name="botao" value="ENVIAR">
         ?>
     </main>
     <footer>
